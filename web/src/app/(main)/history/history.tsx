0a4377fe92e398d8063b77c20549433837b29c26
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-10-31 13:07:09 +0530

    feat: improve design of the history list

--- web/src/app/(main)/history/history.tsx
+++ web/src/app/(main)/history/history.tsx
@@ -4,6 +4,15 @@ import Image from "next/image";
 import Link from "next/link";
 import { cache } from "react";
 import { r2, makeSignedUrl } from "@/lib/r2";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Clock } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 const allGenerations = cache(async (userGoogleId: string) => {
   const generations = await getAllGenerations(userGoogleId);
@@ -23,42 +32,59 @@ const allGenerations = cache(async (userGoogleId: string) => {
   });
 });
 
-export async function History({ userGoogleId }: { userGoogleId: string }) {
-  const gen = await allGenerations(userGoogleId);
-  console.log(gen);
-  return (
-    <>
-      {gen.map((gen) => (
-        <GenerationsPreview generation={gen} key={gen.id} />
-      ))}
-    </>
-  );
-}
-
 type GenerationViewProp = Omit<GenerationViewType[number], "images"> & {
   images: string;
 };
-
-function GenerationsPreview({
+export function GenerationsPreview({
   generation,
 }: {
   generation: GenerationViewProp;
 }) {
   return (
-    <Link href={`/history/${generation.configId}`} className="contents">
-      <div className="max-w-sm bg-card p-4 rounded-lg shadow w-[min(200px,100%)]">
-        <Image
-          src={generation.images}
-          alt="image"
-          width={250}
-          height={250}
-          className="w-full object-cover rounded-lg h-[200px]"
-        />
-        <div className="space-y-2">
-          <h2 className="text-xl font-bold truncate">{generation.topic}</h2>
-          <p className="text-sm">{generation?.duration! / 1000} seconds</p>
-        </div>
-      </div>
+    <Link
+      href={`/history/${generation.configId}`}
+      className="block transition-transform hover:scale-105"
+    >
+      <Card className="overflow-hidden">
+        <CardHeader className="p-0">
+          <div className="relative h-48 min-w-52">
+            <Image
+              src={generation.images}
+              alt={generation.topic!}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        </CardHeader>
+        <CardContent className="p-4">
+          <CardTitle className="text-lg font-bold line-clamp-1">
+            {generation.topic}
+          </CardTitle>
+        </CardContent>
+        <CardFooter className="p-4 pt-0 flex justify-between items-center">
+          <Badge
+            className="flex items-center gap-1 justify-center"
+            variant={"secondary"}
+          >
+            <Clock className="size-4" />
+            <span className="text-sm font-medium flex-1">
+              {(generation?.duration! / 1000).toFixed(1) || "??"}s
+            </span>
+          </Badge>
+        </CardFooter>
+      </Card>
     </Link>
+  );
+}
+
+export async function History({ userGoogleId }: { userGoogleId: string }) {
+  const gen = await allGenerations(userGoogleId);
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-6">
+      {gen.map((gen) => (
+        <GenerationsPreview generation={gen} key={gen.id} />
+      ))}
+    </div>
   );
 }
